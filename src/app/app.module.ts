2dbfbe181f9f5d35bd8103ee4c6fd073ce2bc216
LalitDhane <lalitdhane4455@gmail.com>
2022-09-12 16:01:47 +0530

    added login signUp and Cart components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductCardComponent } from './product-card/product-card.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
-  declarations: [AppComponent, ProductCardComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  declarations: [AppComponent, ProductCardComponent, LoginComponent, SignupComponent, CartComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule,FontAwesomeModule],
   providers: [ProductCardService],
   bootstrap: [AppComponent],
 })
