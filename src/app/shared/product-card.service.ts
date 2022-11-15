20a81674fb472796b043177f10dba0356209f848
Lalit Dhane <lalitdhane4455@gmail.com>
2022-11-15 11:38:26 +0530

    Update API URL

--- src/app/shared/product-card.service.ts
+++ src/app/shared/product-card.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class ProductCardService {
-  url: string = 'https://homemadebags-production.up.railway.app/api/products';
+  url: string = 'https://homemadebags-backend.onrender.com/api/products';
   constructor(private httpClient: HttpClient) {}
 
   public getProducts() {
